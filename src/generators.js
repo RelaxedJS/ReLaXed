c086bf85dbe7f4e1acd2d49617cf3680fb621a5a
Drew Sommer <drwsommer@gmail.com>
2018-05-27 19:26:51 -0600

    Minor formatting

--- src/generators.js
+++ src/generators.js
@@ -4,67 +4,63 @@
  */
 exports.bibliography = async function(page) {
 
-  // Get all the keys from citations
-  var values = await page.$$eval('.citation', nodes => {
-    return nodes.map(node => {
-      return node.getAttribute('data-key')
-    })
-  }).catch(e => {
+    // Get all the keys from citations
+    var values = await page.$$eval('.citation', nodes => {
+        return nodes.map(node => {
+        return node.getAttribute('data-key')
+        })
     // Error occurs because there are no citations
-    return false
-  })
+    }).catch(e => { return false })
 
-  // No citations
-  if (!values) return false
+    // No citations
+    if (!values) return false
 
-  const Cite = require('citation-js')
-  const data = new Cite()
+    const Cite = require('citation-js')
+    const data = new Cite()
 
-  // Add all keys to citation-js
-  values.forEach(val => data.add(val))
+    // Add all keys to citation-js
+    values.forEach(val => data.add(val))
 
-  // Format the citation spans
-  var result = await page.$$eval('.citation', (nodes, data) => {
-    for (var element of nodes) {
-      let key = element.getAttribute('data-key')
-      let page = element.getAttribute('data-page')
-      for (var datum of data) {
-        if (datum.id == key) {
-          if (page != '') {
-            element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]}, p. ${page})`
-          } else {
-            element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]})`
-          }
-          break
+    // Format the citation spans
+    var result = await page.$$eval('.citation', (nodes, data) => {
+        for (var element of nodes) {
+        let key = element.getAttribute('data-key')
+        let page = element.getAttribute('data-page')
+        for (var datum of data) {
+            if (datum.id == key) {
+            if (page != '') {
+                element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]}, p. ${page})`
+            } else {
+                element.innerHTML = `(${datum.author[0].family}, ${datum.issued['date-parts'][0][0]})`
+            }
+            break
+            }
         }
-      }
-    }
-  }, data.data)
+        }
+    }, data.data)
 
-  // Get the bibliography style
-  // TODO: remove with plugin system: define with [//- use-plugin: bibliography <style>] or [config.json]
-  var style = await page.$eval('#bibliography', element => {
-    return element.getAttribute('data-style')
-  }).catch(e => {
+    // Get the bibliography style
+    // TODO: remove with plugin system: define with [//- use-plugin: bibliography <style>] or [config.json]
+    var style = await page.$eval('#bibliography', element => {
+        return element.getAttribute('data-style')
     // Error occurs because there is no bibliography
-    return false
-  })
+    }).catch(e => { return false })
 
-  // No style because no bibliography
-  if (!style) return false
+    // No style because no bibliography
+    if (!style) return false
 
-  // Format html output for bibliography
-  const output = data.get({
-    format: 'string',
-    type: 'html',
-    style: style,
-    lang: 'en-US'
-  })
+    // Format html output for bibliography
+    const output = data.get({
+        format: 'string',
+        type: 'html',
+        style: style,
+        lang: 'en-US'
+    })
 
-  // Set Bibliography
-  await page.$eval('#bibliography', (element, data) => {
-    element.innerHTML = data
-  }, output)
+    // Set Bibliography
+    await page.$eval('#bibliography', (element, data) => {
+        element.innerHTML = data
+    }, output)
 
-  return true
+    return true
 }
